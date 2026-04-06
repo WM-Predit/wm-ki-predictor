9b968796699d179911bf2835b99c8f6652850417
WM-Predit <max.schulz20.ms@gmail.com>
2026-04-06 12:14:14 +0200

    Create Datenschutz.tsx

---
+++ wm-ki-predictor/src/Datenschutz.tsx
@@ -0,0 +1,22 @@
+export default function Datenschutz() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Datenschutzerklärung</h1>
+
+      <p>
+        Diese Website wird bei Vercel gehostet. Beim Aufruf der Website können
+        technische Daten wie IP-Adresse, Browsertyp, Datum und Uhrzeit erfasst werden.
+      </p>
+
+      <p>
+        Diese Daten dienen ausschließlich der Bereitstellung und Sicherheit der Website.
+      </p>
+
+      <p>
+        Kontakt:<br />
+        Maximilian Schulz<br />
+        E-Mail: max.schulz20.ms@gmail.com
+      </p>
+    </div>
+  );
+}
